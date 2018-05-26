acd41693fe912644d2a411260a527e98ef6145e9
alex.fournier <alex.fournier@infusionsoft.com>
2018-05-26 02:06:43 -0700

    Creating Predicate Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,25 @@ public class Main {
 
 
         LeadScoring ls1 = new LeadScoring.LeadScoringBuilder("test@gmail.com").annualrevenue("15645764000").country("USA").email("test@gmail.com")
-                .industry("Software").leadsourcename("John AppleSeed").numberofemployees("1030").yearsinbusiness("4").qualificationscore(1).isconverted(y);
+                .industry("Software").leadsourcename("John AppleSeed").numberofemployees("1030").yearsinbusiness("4").qualificationscore(1).isconverted(true).createLeadScore();
 
         LeadScoring ls2 = new LeadScoring.LeadScoringBuilder("tes1t@gmail.com").annualrevenue("1045747400").country("USA").email("test1@gmail.com")
-                .industry("Software").leadsourcename("Sarah AppleSeed").numberofemployees("1040").yearsinbusiness("4").qualificationscore(1).isconverted(y);
+                .industry("Software").leadsourcename("Sarah AppleSeed").numberofemployees("1040").yearsinbusiness("4").qualificationscore(1).isconverted(true).createLeadScore();
 
         LeadScoring ls3 = new LeadScoring.LeadScoringBuilder("test32424@gmail.com").annualrevenue("15474000").country("USA").email("tes2t@gmail.com")
-                .industry("Software").leadsourcename("Adam AppleSeed").numberofemployees("1030").yearsinbusiness("4").qualificationscore(1).isconverted(y);
+                .industry("Software").leadsourcename("Adam AppleSeed").numberofemployees("1030").yearsinbusiness("4").qualificationscore(1).isconverted(true).createLeadScore();
 
         LeadScoring ls4 = new LeadScoring.LeadScoringBuilder("test5353@gmail.com").annualrevenue("1036300").country("USA").email("test3@gmail.com")
-                .industry("Software").leadsourcename("Clarke AppleSeed").numberofemployees("1003").yearsinbusiness("4").qualificationscore(1).isconverted(n);
+                .industry("Software").leadsourcename("Clarke AppleSeed").numberofemployees("1003").yearsinbusiness("4").qualificationscore(1).isconverted(false).createLeadScore();
 
         LeadScoring ls5 = new LeadScoring.LeadScoringBuilder("test5454@gmail.com").annualrevenue("10456400").country("USA").email("test4@gmail.com")
-                .industry("Software").leadsourcename("Joe AppleSeed").numberofemployees("1400").yearsinbusiness("1").qualificationscore(3).isconverted(y);
+                .industry("Software").leadsourcename("Joe AppleSeed").numberofemployees("1400").yearsinbusiness("1").qualificationscore(3).isconverted(false).createLeadScore();
 
         LeadScoring ls6 = new LeadScoring.LeadScoringBuilder("test5454@gmail.com").annualrevenue("1056400").country("USA").email("test5@gmail.com")
-                .industry("Software").leadsourcename("Dick AppleSeed").numberofemployees("1300").yearsinbusiness("2").qualificationscore(2).isconverted(n);
+                .industry("Software").leadsourcename("Dick AppleSeed").numberofemployees("1300").yearsinbusiness("2").qualificationscore(2).isconverted(false).createLeadScore();
 
         LeadScoring ls7 = new LeadScoring.LeadScoringBuilder("test343@gmail.com").annualrevenue("1043640").country("USA").email("test6@gmail.com")
-                .industry("Software").leadsourcename("Shanna AppleSeed").numberofemployees("1003").yearsinbusiness("6").qualificationscore(1).isconverted(n);
+                .industry("Software").leadsourcename("Shanna AppleSeed").numberofemployees("1003").yearsinbusiness("6").qualificationscore(1).isconverted(false).createLeadScore();
 
 
         ArrayList<LeadScoring> newList = new ArrayList<LeadScoring>();
@@ -40,5 +40,11 @@ public class Main {
 
 
 
+
+
+
     }
+
+
+    public static List
 }
